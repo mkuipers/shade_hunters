59fd35cc71f3cd90b0eb78fe8e32ece71f351677
Max Kuipers <max.kuipers@centro.net>
2014-04-11 14:45:29 -0500

    added character classes

--- app/models/character_card.rb
+++ app/models/character_card.rb
@@ -29,14 +29,48 @@ module CharacterCards
   end
 
   class CharacterCard
+    attr_reader :name, :max_hp, :allegiance, :win_condition, :special_ability
+
+    def hunter?
+      allegiance == 'hunter'
+    end
+
+    def shadow?
+      allegiance == 'shadow'
+    end
+
+    def neutral?
+      allegiance == 'neutral'
+    end
   end
 
   class NeutralCharacterCard < CharacterCard
+    def initialize(name:, max_hp:, win_condition:, special_ability:)
+      @allegiance = 'neutral'
+      @name = name
+      @max_hp = max_hp
+      @win_condition = win_condition
+      @special_ability = special_ability
+    end
   end
 
   class ShadowCharacterCard < CharacterCard
+    def initialize(name:, max_hp:, special_ability:)
+      @allegiance = 'shadow'
+      @name = name
+      @max_hp = max_hp
+      @special_ability = special_ability
+      # set win condition - all hunters dead or 3 neutrals dead
+    end
   end
 
   class HunterCharacterCard < CharacterCard
+    def initialize(name:, max_hp:, special_ability:)
+      @allegiance = 'hunter'
+      @name = name
+      @max_hp = max_hp
+      @special_ability = special_ability
+      # set win condition - all shadows dead
+    end
   end
 end
